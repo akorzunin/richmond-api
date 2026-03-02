5df3725fc847e57a551adbe579a1ca38c3921eab
akorzunin <akorzunin123@gmail.com>
2026-03-02 21:30:52 +0300

    feat: added  goose migrations

--- internal/db/schema/001_users.sql
+++ internal/db/schema/001_users.sql
@@ -1,5 +1,9 @@
+-- +goose Up
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL UNIQUE,
     user_pass VARCHAR(255) NOT NULL
 );
+
+-- +goose Down
+DROP TABLE IF EXISTS users;
